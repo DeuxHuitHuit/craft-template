8ea3f4ffdf26a11b604fd28ded07aad577bf425c
Frederick Hamon <frederick@deuxhuithuit.co>
2020-10-15 08:29:32 -0400

    added language redirect and removed scafolder comments

--- modules/build/src/Build.php
+++ modules/build/src/Build.php
@@ -19,40 +19,11 @@ use yii\base\Event;
 use yii\base\InvalidConfigException;
 use yii\base\Module;
 
-/**
- * Craft plugins are very much like little applications in and of themselves. We’ve made
- * it as simple as we can, but the training wheels are off. A little prior knowledge is
- * going to be required to write a plugin.
- *
- * For the purposes of the plugin docs, we’re going to assume that you know PHP and SQL,
- * as well as some semi-advanced concepts like object-oriented programming and PHP namespaces.
- *
- * https://craftcms.com/docs/plugins/introduction
- *
- * @author    Deux Huit Huit
- * @package   Build
- * @since     0.1.0
- *
- */
 class Build extends Module
 {
-    // Static Properties
-    // =========================================================================
 
-    /**
-     * Static property that is an instance of this module class so that it can be accessed via
-     * Build::$instance
-     *
-     * @var Build
-     */
     public static $instance;
 
-    // Public Methods
-    // =========================================================================
-
-    /**
-     * @inheritdoc
-     */
     public function __construct($id, $parent = null, array $config = [])
     {
         Craft::setAlias('@modules/build', $this->getBasePath());
@@ -63,17 +34,6 @@ class Build extends Module
         parent::__construct($id, $parent, $config);
     }
 
-    /**
-     * Set our $instance static property to this class so that it can be accessed via
-     * Build::$instance
-     *
-     * Called after the module class is instantiated; do any one-time initialization
-     * here such as hooks and events.
-     *
-     * If you have a '/vendor/autoload.php' file, it will be loaded for you automatically;
-     * you do not need to load it in your init() method.
-     *
-     */
     public function init()
     {
         parent::init();
@@ -84,7 +44,6 @@ class Build extends Module
             CraftVariable::class,
             CraftVariable::EVENT_INIT,
             function (Event $event) {
-                /** @var CraftVariable $variable */
                 $variable = $event->sender;
                 $variable->set('build', BuildVariable::class);
             }
