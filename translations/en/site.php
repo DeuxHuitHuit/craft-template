1bfbd6faea84a4c8c8543d75bcaddaf12dda4bd6
Dominic Blain <dominic.blain@gmail.co>
2020-10-30 14:44:08 -0400

    Add translations structure and use keys instead of english version

---
+++ translations/en/site.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+	"error_404_surtitle" => "Page Not Found",
+	"error_404_title" => "404. That’s an error.",
+	"error_404_text" => "The requested URL {url} was not found on this server.",
+
+	"error_offline_title" => "Site Offline",
+	"error_offline_text" => "This website is currently offline.",
+
+	"whoops" => "Whoops!",
+	"error_occured" => "An error occured",
+]
+
+?>
