6db5d9710e6617367742aa9c13677189f3b077ba
Felix Guerin <guerinfelix08@gmail.com>
2020-11-06 11:14:41 -0500

    Remove scaffolder comment

--- modules/twigextensionmodule/TwigExtensionModule.php
+++ modules/twigextensionmodule/TwigExtensionModule.php
@@ -14,40 +14,10 @@ use yii\base\Event;
 use yii\base\InvalidConfigException;
 use yii\base\Module;
 
-/**
- * Craft plugins are very much like little applications in and of themselves. We’ve made
- * it as simple as we can, but the training wheels are off. A little prior knowledge is
- * going to be required to write a plugin.
- *
- * For the purposes of the plugin docs, we’re going to assume that you know PHP and SQL,
- * as well as some semi-advanced concepts like object-oriented programming and PHP namespaces.
- *
- * https://craftcms.com/docs/plugins/introduction
- *
- * @author    Deux Huit Huit
- * @package   Build
- * @since     0.1.0
- *
- */
 class TwigExtensionModule extends Module
 {
-    // Static Properties
-    // =========================================================================
-
-    /**
-     * Static property that is an instance of this module class so that it can be accessed via
-     * TwigExtensionModule::$instance
-     *
-     * @var TwigExtensionModule
-     */
     public static $instance;
 
-    // Public Methods
-    // =========================================================================
-
-    /**
-     * @inheritdoc
-     */
     public function __construct($id, $parent = null, array $config = [])
     {
         Craft::setAlias('@modules/twigextensionmodule', $this->getBasePath());
