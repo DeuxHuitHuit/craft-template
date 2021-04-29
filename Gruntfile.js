e55f3753573a9f6274a62346ce6e69a2e1b0f25c
f-elix <guerinfelix08@gmail.com>
2021-04-29 15:10:29 -0400

    Setup tailwind jit mode

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,44 +10,38 @@ module.exports = (grunt) => {
 
 	const tasks = grunt.file.expand({ filter: 'isFile', cwd: 'tasks' }, ['*']);
 
-	tasks.forEach(task => {
+	tasks.forEach((task) => {
 		require(`./tasks/${task}`)(grunt);
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-purgecss');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-csso');
 	grunt.loadNpmTasks('grunt-ftp-push');
 	grunt.loadNpmTasks('grunt-babel');
-	grunt.loadNpmTasks('grunt-complexity');
+	grunt.loadNpmTasks('grunt-env');
 
-	grunt.registerTask('dev-js', ['complexity', 'jshint']);
+	grunt.registerTask('dev-js', ['jshint']);
 	grunt.registerTask('dev', ['dev-js']);
 
-	grunt.registerTask('js', ['dev-js', 'clean:js', 'libs', 'concat:js', 'babel:es5', 'babel:modern', 'headers:js']);
-	grunt.registerTask('css', [
-		'clean:css',
-		'postcss:build',
-		'concat:css',
-		'purgecss',
-		'csso',
-		'headers:css'
+	grunt.registerTask('js', [
+		'dev-js',
+		'clean:js',
+		'libs',
+		'concat:js',
+		'babel:es5',
+		'babel:modern',
+		'headers:js'
 	]);
-	grunt.registerTask('css-dev', ['clean:css', 'postcss', 'ftp_push:dev']);
-	grunt.registerTask('build', ['buildnum', 'css', 'js']);
+	grunt.registerTask('css', ['clean:css', 'postcss:build', 'concat:css', 'csso', 'headers:css']);
+	grunt.registerTask('css-dev', ['clean:css', 'postcss:build', 'ftp_push:dev']);
+	grunt.registerTask('build', ['env:build', 'buildnum', 'css', 'js']);
 
 	grunt.registerTask('push', ['ftp_push:css', 'ftp_push:js', 'ftp_push:build']);
 
-	grunt.registerTask('setup', [
-		'clean:css-utils',
-		'postcss:utils',
-		'ftp_push:dev',
-		'build',
-		'push'
-	]);
+	grunt.registerTask('setup', ['clean:css', 'postcss:build', 'ftp_push:dev', 'build', 'push']);
 
-	grunt.registerTask('default', ['watch']);
+	grunt.registerTask('default', ['env:dev', 'watch']);
 };
--- tasks/watch.js
+++ tasks/watch.js
@@ -3,18 +3,13 @@ module.exports = (grunt) => {
 
 	grunt.config.merge({
 		watch: {
-			twig: {
-				files: ['templates/**/*.twig'],
-				tasks: ['noop']
-			},
-			css: {
-				files: ['web/assets/css/**/*.css', '!web/assets/css/pre-build/**'],
-				tasks: ['clean:css-main', 'postcss:main', 'ftp_push:dev']
-			},
-			tailwind: {
-				files: ['tailwind.config.js', '!web/assets/css/pre-build/**'],
-				tasks: ['clean:css-utils', 'postcss:utils', 'ftp_push:dev']
-			},
+			files: [
+				'templates/**/*.twig',
+				'tailwind.config.js',
+				'web/assets/css/**/*.css',
+				'!web/assets/css/pre-build/**'
+			],
+			tasks: ['clean:css', 'postcss:build', 'ftp_push:dev'],
 			options: {
 				spawn: false,
 				livereload: true,
